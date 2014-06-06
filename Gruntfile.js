79a9961e571e69f8369b57a4f472705e3a0cfada
Renaud Fontana <sirgzu@hotmail.com>
2014-06-06 17:08:54 +0100

    changed banner

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,11 +5,11 @@ module.exports = function (grunt) {
 	grunt.initConfig({
 		// Metadata.
 		pkg: grunt.file.readJSON('package.json'),
-		banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+		banner: '/* <%= pkg.name %> - v<%= pkg.version %> - ' +
 			'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-			'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-			'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-			' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+			'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %> ' +
+			'* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+			'<%= pkg.author %>\n * Licensed <%= pkg.license %> */\n',
 		// Task configuration.
 		clean: {
 			files: ['dist']
