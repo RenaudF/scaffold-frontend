8a6c5ac7a147e1bb1e9e64db75b805cf6a36b920
Renaud Fontana <sirgzu@hotmail.com>
2014-06-06 17:21:02 +0100

    rewrote example e2e test
    now including Q promises

--- test/test.e2e.js
+++ test/test.e2e.js
@@ -1,45 +1,39 @@
 /*global browser, By */
-var driver = browser.driver;
 
 
+var Q = require('q');
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 
+var assert = chai.assert;
 var expect = chai.expect;
-describe('expect syntax', function() {
-	it('should be Running!', function() {
-		var heading = driver.findElement(By.tagName('h1'));
-		expect(heading.getText()).to.eventually.eql('Running!');
-	});
-});
-
 chai.should();
-describe('synchronous should syntax', function() {
-	it('should be Running!', function() {
-		var heading = driver.findElement(By.tagName('h1'));
-		heading.getText().then(function(text){
-			text.should.eql('Running!');
-		});
-	});
-});
 
-describe('asynchronous should syntax', function() {
-	it('should be Running!', function() {
-		var heading = driver.findElement(By.tagName('h1'));
-		heading.getText().should.eventually.eql('Running!');
-	});
+var driver = browser.driver;
+
 before(function(){
 	driver.get('http://localhost:'+browser.params.port);
-});
+	
 });
 
-var assert = chai.assert;
-describe('assert syntax', function() {
-	it('should be Running!', function() {
+describe('Testing environment', function(){
+	it('should work with the "assert" syntax', function(){
 		var heading = driver.findElement(By.tagName('h1'));
 		heading.getText().then(function(text){
 			assert.equal(text, 'Running!');
 		});
+	});
+	it('should work with the "expect" syntax', function() {
+		var heading = driver.findElement(By.tagName('h1'));
+		expect(heading.getText()).to.eventually.eql('Running!');
+	});
+	it('should work with the "should" syntax', function() {
+		var heading = driver.findElement(By.tagName('h1'));
+		// Note that webdriver promises must be wrapped in a Q promise
+		// in order to be used with the 'eventually' sugar syntax.
+		// This is due to chai-as-promised not supporting the same
+		// promise implementation as webdriverjs and protractor.
+		new Q(heading.getText()).should.eventually.eql('Running!');
 	});
 });
