522b2992a45ec3db03b5a8ce7a81afddb252d942
Renaud Fontana <sirgzu@hotmail.com>
2014-06-12 16:43:21 +0100

    fixed backend asynchronous tests
    tests were passing that should have failed.

--- backend/app/module.js
+++ backend/app/module.js
@@ -5,6 +5,6 @@ var Q = require('q');
 var deferred = Q.defer();
 setTimeout(function(){
 	deferred.resolve('async module');
-}, 1000);
+}, 100);
 
 module.exports = deferred.promise;
--- backend/test/test.unit.js
+++ backend/test/test.unit.js
@@ -9,15 +9,13 @@ var expect = chai.expect;
 chai.should();
 
 describe('Testing environment', function(){
-	it('should work with the "assert" syntax', function(){
-		require('../app/module').then(function(text){
-			assert.equal(text, 'Running!');
-		});
+	it('should work with the "assert" syntax', function(done){
+		assert.eventually.equal(require('../app/module'), 'async module').notify(done);
 	});
-	it('should work with the "expect" syntax', function() {
-		expect(require('../app/module')).to.eventually.equal('async module');
+	it('should work with the "expect" syntax', function(done) {
+		expect(require('../app/module')).to.eventually.equal('async module').notify(done);
 	});
-	it('should work with the "should" syntax', function(){
-		require('../app/module').should.eventually.equal('async module');
+	it('should work with the "should" syntax', function(done){
+		require('../app/module').should.eventually.equal('async module').notify(done);
 	});
 });
