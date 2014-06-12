dc3fdcfbe450ea1e0e473f8ca6b62c7ae082f4de
Renaud Fontana <sirgzu@hotmail.com>
2014-06-12 16:42:05 +0100

    added frontend asynchronous unit tests

--- frontend/test/test.unit.js
+++ frontend/test/test.unit.js
@@ -2,11 +2,12 @@
 
 // Define your unit tests in requirejs modules
 // You can inject any module from your app within the tests.
-define(['chai', 'libs/d3/d3', 'config'], function (chai, d3, q) {
+define(['chai', 'libs/q/q'], function (chai, q) {
 	// chai must be required through requirejs, chai-as-promised
 	// sinon and should are already exported to the global scope
 	var assert = chai.assert;
 	var expect = chai.expect;
+
 	describe('Testing environment', function () {
 		it('should work with chai', function () {
 			assert.equal('test', 'test');
@@ -17,11 +18,21 @@ define(['chai', 'libs/d3/d3', 'config'], function (chai, d3, q) {
 			var spy = sinon.spy();
 			spy.should.not.have.been.called;
 		});
-		it('should find modules', function(){
-			config.should.exist;
+		it('should find modules', function(done){
+			// using deferred syntax
+			var deferred = q.defer();
+			require(['config'], function(config){
+				deferred.resolve(config);
+			});
+			deferred.promise.should.eventually.be.fulfilled.notify(done);
 		});
-		it('should find dependencies', function(){
-			d3.should.exist;
+		it('should find dependencies', function(done){
+			// using promise syntax
+			q.promise(function(resolve, reject, notify){
+				require(['libs/d3/d3'], function(d3){
+					resolve(d3);
+				});
+			}).should.eventually.be.fulfilled.notify(done);
 		});
 	});
 });
